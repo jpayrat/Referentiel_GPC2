06d97474dc051d6fae6f10fdcc6d9115c319ab98
jpayrat <julien.payrat@orange.com>
2016-07-17 19:26:51 +0200

    Relexion sur la structure
    Il va falloir revoir l’architecture de notre application, pour l’admin
    c’est la galère pour retrouver les dossiers d’utilisation, et la base,
    en gros les infos qu’on récupère de l’URL. Je pense à récupérer les
    infos de l’URL, l’envoyer au contrôleur de base et traiter des choses,
    mais a voir, faut que j’y réfléchisse !!!

--- _controleurs/adminControleur.php
+++ _controleurs/adminControleur.php
@@ -2,6 +2,7 @@
 
 namespace RefGPC\_controleurs;
 use \RefGPC\_models\ilot\ModelVue;
+use \RefGPC\_models\ilot\Formulaire;
 
 /**
  * Description of adminControleur
@@ -16,14 +17,18 @@ class adminControleur extends baseControleur{
 
 
     public  function affIndex($params) {
+
+        $form = new Formulaire($this->codeBase); // choixBase->codeBase());
+        $this->d['corps']['inputIlotGlobal'] = $form->input('rechercheIlotGlobal', '30','28');
+        $this->d['corps']['inputIlotTape'] = $form->input('rechercheIlotTape','3','3');
+
         // var_dump($this->d);
         $vue = new ModelVue($this->d);
 
         $vue->afficheHaut($this->d['haut'], ''); // Le second paramètre = fichier js à inclure
 //        $vue->afficheMenuLateral($this->d['lateral']);
 
-        //$vue->afficheCorps($this->d['corps'], 'affIndexIlot');//
-
+        $vue->afficheCorps($this->d['corps'], 'affIndexAdmin');// TODO : je n'arrive pas à automatiser le "affIndex"
         $vue->afficheBas();
     }
 
---
+++ _vues/affIndexAdmin.php
@@ -0,0 +1,91 @@
+<div class="container" id="ilot_corps">
+
+    <form method="post" id="form_ilot" action="#" class="formulaire">
+
+        <h1> Îlots GPC <?php echo '- ' . $libelleBase; ?><span class=""><?= $nbIlot>0 ? ' ('.$nbIlot.' ilots' : ''; ?>)</span></h1>
+        <hr />
+
+        <label for="rechercheGlobal" class="gen">Recherche globale</label>
+        <?= $inputIlotGlobal; ?>
+
+        <div style="border: 0px solid #000; display: inline-block;text-align: center; width: 52%;">
+            <span id="all_form">Lister tous les îlots</span>
+            <span id="reinit_form">Réinitialiser le formulaire</span>
+        </div>
+
+        <br />
+
+        <label for="recherchedetail" class="gen" id="search_ilot_detail">Recherche avancée</label><span id="arrow" class="arrowBottom"></span>
+
+        <div class="search_ilot_detail">
+            <div style="width: 55%; display: inline-block;">
+                <label for="ilot_tape" class="gen">Ilot </label>
+                <?= $inputIlotTape; ?>
+                <span>ou</span>
+                <?= $selectIlotList; ?>
+            </div>
+            
+            <div style="width: 20%; display: inline-block;">
+                <label for="typeIlot" class="gen">Type d'îlot </label>
+                <?= $selectTypeIlot; ?>
+            </div>
+            
+            <div style="width: 19%; display: inline-block;">
+                <label for="used" class="gen">Utilisé ? </label>
+                <?= $selectUsed; ?>
+            </div>
+
+            <br />
+
+            <div class="ligne_form">
+                <label for="competence" class="gen">Compétence de l'îlot </label>
+                <?= $selectCompetence; ?>
+            </div>
+
+            <div class="ligne_form">
+                <label for="serviceCible" class="gen">Service concerné </label>
+                <?= $selectServiceCible; ?>
+            </div>
+
+            <br />
+
+            <div class="ligne_form">
+                <label for="entreprise" class="gen">Entreprise</label>
+                <?= $selectEntreprise; ?>
+            </div>
+
+            <div class="ligne_form">
+                <label for="siteGeo" class="gen">Site géographique </label>
+                <?= $selectSiteGeo; ?>
+            </div>
+
+            <br />
+
+            <div class="ligne_form">
+                <label for="domaineAct" class="gen">Domaine d'activité </label>
+                <?= $selectDomaineAct; ?>
+            </div>
+
+            <div class="ligne_form_radio">
+                <label for="optim">Ilot Optimisé </label>
+                <input type="radio" id="optim_ok" value="1" name="optim" />
+                <label for="optim_ok" class="radio">Oui</label>
+
+                <input type="radio" id="optim_nok" value="0" name="optim" />
+                <label for="optim_nok" class="radio">Non</label>
+
+                <input type="radio" id="optim_tout" value="tous" name="optim" checked />
+                <label for="optim_tout" class="radio">Les deux</label>
+            </div>
+
+        </div>
+        <br />
+        <br />
+    </form>
+
+    <!-- div du retour de la requete Ajax -->
+    <div id="results_ilot" style="display: none;"></div>
+    <br />
+
+</div>
+</section>
